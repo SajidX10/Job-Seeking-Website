582ec68a3429710a7ce20255c432c3817ca11d6b
Mohammad Sajidul Islam <140744541+SajidX10@users.noreply.github.com>
2024-12-27 03:08:03 +0600

    Delete backend directory

--- backend/generateObjectId.js
+++
@@ -1,5 +0,0 @@
-const { ObjectId } = require('mongodb');
-
-// Generate a new ObjectId
-const validObjectId = new ObjectId();
-console.log("Generated ObjectId:", validObjectId.toString());
--- backend/routes/interviewRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { scheduleInterview, getInterviews } from "../controllers/interviewController.js";
-
-const router = express.Router();
-
-router.post("/", scheduleInterview);
-router.get("/:candidateId", getInterviews);
-
-export default router;
